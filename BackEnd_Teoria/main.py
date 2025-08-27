4bab43b24570f65df55a194129ef3d8551741d43
Julia Laurindo <160611630+j-laurindo@users.noreply.github.com>
2025-08-27 14:33:34 -0300

    Finalização Main

--- BackEnd_Teoria/main.py
+++ BackEnd_Teoria/main.py
@@ -1,6 +1,9 @@
 from tamagoshi import *
 
 def main():
+    ##############################################################
+    #  MENU ENTRADA | Nome do usuário + Criação do Tamagoshi
+    ##############################################################
     print("\n-------------------------------------------------")
     print("|   BEM VINDO(A)! BRINQUE COM O SEU TAMAGOSHI   |")
     print("-------------------------------------------------")
@@ -9,13 +12,24 @@ def main():
     resposta = int(input("Gostaria de criar um tamagoshi para você?    \n1 = SIM  |  2 = NÃO \nResposta: "))
     print("-------------------------------------------------")
 
+    ##############################################################
+    #  MENU ENTRADA | Resposta = Não
+    ##############################################################
     if(resposta == 2):
         print("\n-------------- FINALIZANDO JOGO --------------")
         exit()
+    
+    ##############################################################
+    #  MENU ENTRADA | Resposta = Sim
+    ##############################################################
     else:
+        ##############################################################
+        #  MENU DE CRIAÇÃO DO TAMAGOSHI
+        ##############################################################
         print("\n -> Ótimo! Escolha qual tamagoshi deseja adotar: ")
         escolhaTamagoshi = int(input("1 = Targaryen 🐉\n2 = Garfield 🐱\n3 = Vampiro 🧛\n-> Qual será sua escolha: "))
 
+        ## Opção 1 = Criar o Tamagoshi Targaryen ##
         if(escolhaTamagoshi == 1):
             time.sleep(1)
             print("\n--- CRIANDO SEU TAMAGOSHI TARGARYEN ---")
@@ -32,6 +46,8 @@ def main():
             print("-------")
             time.sleep(1)
             print("---------------")
+
+        ## Opção 2 = Criar o Tamagoshi Garfield ##
         elif(escolhaTamagoshi == 2):
             time.sleep(1)
             print("\n--- CRIANDO SEU TAMAGOSHI GARFIELD ---")
@@ -48,6 +64,8 @@ def main():
             print("-------")
             time.sleep(1)
             print("---------------")
+        
+        ## Opção 3 = Criar o Tamagoshi Vampiro ##
         elif(escolhaTamagoshi == 3):
             time.sleep(1)
             print("\n--- CRIANDO SEU TAMAGOSHI VAMPIRO ---")
@@ -64,6 +82,8 @@ def main():
             print("-------")
             time.sleep(1)
             print("---------------")
+        
+        ## Opção Errada = Finalização do jogo ##
         else:
             time.sleep(1)
             print("\n-> Nenhuma opção correta foi selecionada, devido a isso o programa será finalizado")
@@ -78,7 +98,12 @@ def main():
             print("\n----- FIM DO JOGO -----")
         
         opcao = 1
+
+        ##############################################################
+        #  JOGO DO TAMAGOSHI
+        ##############################################################
         while(opcao != 0):
+            ## Verificação da vida do Tamagoshi ##
             if(tamagoshi.saude <= 0):
                 print(f"------- {nomeTamagoshi} está morrendo! -------")
                 time.sleep(1)
@@ -92,7 +117,10 @@ def main():
                 time.sleep(1)
                 print(f"\n{nomeTamagoshi} faleceu :(")
                 break
-
+            
+            ##############################################################
+            #  Menu | JOGO DO TAMAGOSHI 
+            ##############################################################
             tamagoshi.vida()
             print("\n1 -> Alimentar")
             print("2 -> Brincar")
@@ -101,6 +129,137 @@ def main():
             print("0 -> Sair do Jogo")
 
             opcao = int(input("\n- Digite sua opção: "))
+
+            #### Opção | Alimentar ####
+            if(opcao == 1):
+                comida = int(input(f"\n-- O quanto de comida você dará ao {nomeTamagoshi}? -- \n-> Resposta: "))
+                tamagoshi.alimentar(comida)
+                print(f"\n-> {nomeTamagoshi}: Hummmmm, que delícia! Muito obrigada, {nomeUsuario}!")
+                print()
+                tamagoshi.status()
+
+            #### Opção | Brincar ####
+            elif(opcao == 2):
+                xp = int(input(f"\n-- O quanto de xp você dará ao {nomeTamagoshi} durante a brincadeira? -- \n-> Resposta: "))
+                tamagoshi.brincar(xp)
+                print(f"\n-> {nomeTamagoshi}: Obrigado pela brincadeira, {nomeUsuario}!")
+                print()
+                tamagoshi.status()
+            
+            #### Opção | Ações Específicas ####
+            elif(opcao == 3):
+                ################################################
+                # Escolha = Tamagoshi Targaryen
+                ################################################
+                if(escolhaTamagoshi == 1):
+                    print(f"\n-- ESCOLHA SUA AÇÃO PARA {nomeTamagoshi} TARGARYEN --")
+                    print("-> 1 = Nomear Dragão")
+                    print("-> 2 = Voar com Dragão")
+                    print("-> 3 = Treinar Valiriano")
+                    print("-> 4 = Lutar pelo Trono de Ferro")
+                    escolha = int(input("\n---> Resposta: "))
+                    if(escolha == 1):
+                        print()
+                        tamagoshi.nomearDragao()
+                        print()
+                        tamagoshi.status()
+                    elif(escolha == 2):
+                        tempo = int(input(f"-> Digite por quanto tempo {nomeTamagoshi} Targaryen voou com {tamagoshi.dragao}"))
+                        tamagoshi.voarDragao(tempo)
+                        print()
+                        tamagoshi.status()
+                    elif(escolha == 3):
+                        print()
+                        tamagoshi.treinarValiriano()
+                        print()
+                        tamagoshi.status()
+                    elif(escolha == 4):
+                        print()
+                        tamagoshi.lutarTrono()
+                        print()
+                        tamagoshi.status()
+                    else:
+                        print("-> Valor Inválido! Escolha uma opção existente")
+                ################################################
+                # Escolha = Tamagoshi Garfield
+                ################################################
+                elif(escolhaTamagoshi == 2):
+                    print(f"\n-- ESCOLHA SUA AÇÃO PARA {nomeTamagoshi} GARFIELD --")
+                    print("-> 1 = Roubar Lasanha")
+                    print("-> 2 = Irritar o Tod")
+                    print("-> 3 = Brincar com o Odie")
+                    escolha = int(input("\n---> Resposta: "))
+                    if(escolha == 1):
+                        print()
+                        tamagoshi.roubarLasanha()
+                        print()
+                        tamagoshi.status()
+                    elif(escolha == 2):
+                        print()
+                        tamagoshi.irritarTod()
+                        print()
+                        tamagoshi.status()
+                    elif(escolha == 3):
+                        tempo = int(input(f"-> Digite por quanto tempo {nomeTamagoshi} Garfield brincou com o Odie"))
+                        tamagoshi.brincarOdie(tempo)
+                        print()
+                        tamagoshi.status()
+                    else:
+                        print("-> Valor Inválido! Escolha uma opção existente")
+                ################################################
+                # Escolha = Tamagoshi Vampiro
+                ################################################
+                elif(escolhaTamagoshi == 3):
+                    print(f"\n-- ESCOLHA SUA AÇÃO PARA {nomeTamagoshi} VAMPIRO --")
+                    print("-> 1 = Tomar Sangue")
+                    print(f"-> 2 = Hipnotizar {nomeUsuario}")
+                    print("-> 3 = Transformar em Morcego")
+                    escolha = int(input("\n---> Resposta: "))
+                    if(escolha == 1):
+                        qtd_sangue = int(input(f"-> Digite a quantidade de sangue que {nomeTamagoshi} vampiro irá tomar: "))
+                        print()
+                        tamagoshi.alimentar(qtd_sangue)
+                        print()
+                        tamagoshi.status()
+                    elif(escolha == 2):
+                        print()
+                        tamagoshi.hipnose()
+                        print()
+                        tamagoshi.status()
+                    elif(escolha == 3):
+                        print()
+                        tamagoshi.transformarMorcego()
+                        print()
+                        tamagoshi.status()
+                    else:
+                        print("-> Valor Inválido! Escolha uma opção existente")
+            
+            #### Opção | Status de Vida ####
+            elif(opcao == 4):
+                tamagoshi.status()
+            
+            #### Opção | Sair do jogo ####
+            elif(opcao == 0):
+                print(f"\n{nomeTamagoshi}: Muito obrigado por jogar comigo, {nomeUsuario}!")
+                time.sleep(1)
+                print("..............")
+                time.sleep(1)
+                print("...........")
+                time.sleep(1)
+                print(".......")
+                time.sleep(1)
+                print("....")
+                time.sleep(1)
+                print(".")
+                time.sleep(1)
+                print("\n------ JOGO ENCERRADO ------")
+            
+            #### Opção Errada ####
+            else:
+                print("-> Valor inválido! Escolha a opção correta")
+            
+            ## Função do tempo passando ##
+            tamagoshi.tempoPassando()
             
 
 main()
